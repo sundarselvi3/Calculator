1e29a866d9ceaf99318ba547d6a9fec250b17936
Admin <sundar.k.selvi3@gmail.com>
2019-03-11 09:50:02 +0530

    added two numbers

--- app/src/main/java/codebind/example/calculator/MainActivity.kt
+++ app/src/main/java/codebind/example/calculator/MainActivity.kt
@@ -2,11 +2,23 @@ package codebind.example.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       var a= findViewById<EditText>(R.id.textView)
+        var b= findViewById<EditText>(R.id.textView2)
+        var c=findViewById<EditText>(R.id.textView3)
+        var btn=findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
+            var a1=a.text.toString().toInt()
+            var b1=b.text.toString().toInt()
+            var test = Test1()
+            c.setText(test.add(a1,b1).toString())
+        }
     }
 }
---
+++ app/src/main/java/codebind/example/calculator/Test1.kt
@@ -0,0 +1,9 @@
+package codebind.example.calculator
+class Test1
+{
+    fun add(num1:Int,num2:Int) :Int
+    {
+        var c :Int=num1+num2
+        return c
+    }
+}
